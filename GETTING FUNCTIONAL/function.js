6fc7f8e50e0134d0b31f89bfb23eed001280fb89
poohbears123 <jmspinuela2020@gmail.com>
2025-03-19 20:57:12 -0700

    Add files via upload

---
+++ GETTING FUNCTIONAL/function.js
@@ -0,0 +1,31 @@
+var dogName = "rover";
+var dogWeight = 23;
+if (dogWeight > 20){
+    console.log (dogName + " says WOF WOF ");
+} else{
+    console.log(dogName + " light ass dog ") 
+}
+
+var dogName = "spot";
+var dogWeight = 13;
+if (dogWeight > 20){
+    console.log (dogName + " says WOF WOF ");
+} else{
+    console.log(dogName + " light ass dog ") 
+}
+
+var dogName = "elsa";
+var dogWeight = 10;
+if (dogWeight > 20){
+    console.log (dogName + " says WOF WOF ");
+} else{
+    console.log(dogName + " light ass dog ") 
+}
+
+var dogName = "adik";
+var dogWeight = 100;
+if (dogWeight > 20){
+    console.log (dogName + " says WOF WOF ");
+} else{
+    console.log(dogName + " light ass dog ") 
+}
---
+++ GETTING FUNCTIONAL/function_v2.js
@@ -0,0 +1,11 @@
+function bark(name, weight){
+    if (dogWeight > 20){
+        console.log (dogName + " says WOF WOF ");
+    } else{
+        console.log(dogName + " light ass dog ");
+    }
+}
+bark("rover",23);
+bark("sticks",10);
+bark("elmo",15);
+bark("lols",100);
