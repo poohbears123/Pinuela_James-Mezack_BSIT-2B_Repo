fcda968a78f627071dd758d40aa0c0a89a82131e
poohbears123 <jmspinuela2020@gmail.com>
2025-03-24 11:24:18 +0800

    Your commit message

---
+++ Activity/function activity/scrip.js
@@ -0,0 +1,15 @@
+
+function WhatShallIWear(temp) {
+    
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    } else if (temp < 70) {
+        console.log("Wear a sweater");
+    } else {
+        console.log("Wear a t-shirt");
+    }
+}
+
+WhatShallIWear(50);  
+WhatShallIWear(60);  
+WhatShallIWear(80);  
---
+++ battleships/battleship v 3.js
@@ -0,0 +1,45 @@
+//variables
+
+var randomLoc = Math.floor(Math.random()*5);
+
+var location1 = randomLoc;
+var location2 =location1 + 1;
+var location3 = location2 + 1;
+
+var guess;
+var hits =  0;
+var guesses = 0;
+
+var isSunk = false;
+
+//main game
+
+while(isSunk == false ){
+    guess = prompt("Ready, Aim, Fire(enter number)")
+    if (guess==null){
+        alert("Thanks for playing");
+        break;
+    }
+
+    if (guess <0 || guess >6){
+        alert ("please put a valid number")
+    }
+    else{ guesses = guesses + 1 
+        if (guess == location1|| guess == location2 || guess == location3){
+            hits += 1;
+            if (hits == 3 ){
+                isSunk = true;
+                alert (" you sank my ship")
+            }else{
+                alert("hit")
+            }}else{
+                alert("miss")
+            }
+        }
+    }
+//acuracy counter
+
+var accuracy = (guess > 0 ?(hits/guesses)* 100:  0). toFixed(2);
+var state = "you sank my battleship at " + guesses + " guesses" + " and your accuracy is " + accuracy + "%"
+
+alert(state)
